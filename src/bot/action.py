b4420ca7bb8acc8d57d657f6af352a85233fd0db
Pbatch <peter_batchelor1@hotmail.com>
2021-10-27 15:21:45 +0100

    Adding base action class

---
+++ src/bot/action.py
@@ -0,0 +1,12 @@
+class Action:
+    def __init__(self, index, tile_x, tile_y, name, cost, type_, target):
+        self.index = index
+        self.tile_x = tile_x
+        self.tile_y = tile_y
+        self.name = name
+        self.cost = cost
+        self.type = type_
+        self.target = target
+
+    def __repr__(self):
+        return f'{self.name} at ({self.tile_x}, {self.tile_y})'
