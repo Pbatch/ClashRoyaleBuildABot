ca2a241f16ad775c98d4acd670d802b16c267d40
Pbatch <peter_batchelor1@hotmail.com>
2021-11-05 00:44:17 +0000

    Making pete bot more sophisticated

--- src/bot/pete/pete_bot.py
+++ src/bot/pete/pete_bot.py
@@ -1,9 +1,10 @@
 from src.bot.bot import Bot
 from src.bot.pete.pete_action import PeteAction
-import numpy as np
 import random
-from copy import deepcopy
 import time
+import os
+import cProfile
+import pstats
 
 
 class PeteBot(Bot):
@@ -14,31 +15,29 @@ class PeteBot(Bot):
         """
         Perform preprocessing on the state
 
-        We retrieve a list of the enemy units and their approximate (x, y) coordinates
+        Estimate the tile of each unit to be the bottom of their bounding box
         """
-        enemy_units = {k: [deepcopy(unit) for unit in v if unit['confidence'] > 0.6]
-                       for k, v in self.state['units'].items()
-                       if k[:5] == 'enemy'}
-        for k, v in enemy_units.items():
+        for k, v in self.state['units'].items():
             for unit in v:
                 bb = unit['bounding_box']
                 bb_box_bottom = [((bb[0] + bb[2]) / 2), bb[3]]
                 unit['tile_xy'] = self._get_nearest_tile(*bb_box_bottom)
-        return enemy_units
 
     def _calculate_action_scores(self, actions):
         """
         Calculate a score for every action
         """
-        enemy_units = self._preprocess()
-        action_scores = [a.calculate_score(enemy_units) for a in actions]
+        self._preprocess()
+        action_scores = [a.calculate_score(self.state['units']) for a in actions]
         return action_scores
 
     def run(self):
-        import os
         i = 0
         os.makedirs('screenshots', exist_ok=True)
         while True:
+            # Setup cProfile
+            profiler = cProfile.Profile()
+            profiler.enable()
             # Set the state of the game
             self.set_state()
             # Obtain a list of playable actions
@@ -46,17 +45,23 @@ class PeteBot(Bot):
             if actions:
                 # Shuffle the actions (because action scores might be the same)
                 random.shuffle(actions)
-                # Calculate a score for each action
-                scores = self._calculate_action_scores(actions)
-                # Choose the best action
-                best_idx = np.argmax(np.array(scores))
-                action = actions[best_idx]
+                # Preprocessing
+                self._preprocess()
+                # Get the best action
+                action = max(actions, key=lambda x: x.calculate_score(self.state['units']))
+                # Skip the action if it doesn't score high enough
+                if action.score[0] == 0:
+                    continue
                 # Play the best action
                 self.play_action(action)
                 # Log the result
-                print(f'Playing {action}')
+                print(f'Playing {action} with score {action.score}')
                 # Take a screenshot
                 screenshot = self.screen.take_screenshot()
                 screenshot.save(f'screenshots/{i}.jpg')
                 i += 1
+                # Show the cProfile results
+                profiler.disable()
+                stats = pstats.Stats(profiler).sort_stats('cumtime')
+                stats.print_stats(25)
             time.sleep(3)
