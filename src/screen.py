3bfc08fb0c2d390db35ea7b2b66720d0dd184797
Pbatch <peter_batchelor1@hotmail.com>
2022-02-05 14:54:26 +0000

    Use mss library to take screenshots (much faster)

--- src/screen.py
+++ src/screen.py
@@ -1,57 +1,75 @@
-from PIL import ImageGrab, ImageOps
-import win32gui
-from win32api import GetSystemMetrics, MAKELONG
-from win32con import WM_LBUTTONDOWN, WM_LBUTTONUP, MK_LBUTTON
 import numpy as np
-from src.data.constants import APP_WIDTH, APP_HEIGHT, BORDER_SIZE
+import win32gui
+from PIL import Image
+from mss import mss
+from win32api import MAKELONG
+from win32con import WM_LBUTTONUP, WM_LBUTTONDOWN, MK_LBUTTON
+
+from src.data.constants import APP_WIDTH, APP_HEIGHT, BORDER_SIZE, SCREEN_WIDTH
 
 
 class Screen:
     def __init__(self):
         self.bluestacks_id = win32gui.FindWindow(None, 'BlueStacks')
-        self.system_width = GetSystemMetrics(0)
-        self.system_height = GetSystemMetrics(1)
 
+    @staticmethod
+    def take_screenshot():
+        """
+        Take a screenshot of the emulator
+
+        You MUST call `move_and_resize_emulator` first
+        """
+        bbox = (SCREEN_WIDTH - APP_WIDTH,
+                BORDER_SIZE,
+                SCREEN_WIDTH - BORDER_SIZE,
+                APP_HEIGHT)
+        with mss() as sct:
+            screenshot = sct.grab(bbox)
+
+        screenshot = np.array(screenshot, dtype=np.uint8)
+        screenshot = np.flip(screenshot[:, :, :3], 2)
+        screenshot = Image.fromarray(screenshot)
+
+        return screenshot
+
+    def _set_foreground_window(self):
+        """
+        Bring the emulator to the foreground
+        """
         win32gui.SetForegroundWindow(self.bluestacks_id)
 
-    def move_and_resize_emulator(self):
+    def _move_and_resize_emulator(self):
         """
         Move the emulator to the top-right of the screen
+        Resize the emulator to APP_WIDTH x APP_HEIGHT
         """
-        # Move the window to the top-right of the screen
-        # Resize to APP_WIDTH x APP_HEIGHT
         win32gui.MoveWindow(self.bluestacks_id,
-                            self.system_width - APP_WIDTH,
+                            SCREEN_WIDTH - APP_WIDTH,
                             0,
                             APP_WIDTH,
                             APP_HEIGHT,
                             True)
-
-    def take_screenshot(self):
-        """
-        Take a screenshot of the emulator and crop out the BlueStacks border
-        """
-        screen = win32gui.GetWindowRect(self.bluestacks_id)
-        screenshot = ImageGrab.grab(screen)
-
-        # The border goes from the top-left, to the top-right, to the bottom-right
-        border = (0, BORDER_SIZE, BORDER_SIZE, 0)
-        screenshot = ImageOps.crop(screenshot, border)
-        return screenshot
 
     def click(self, x, y):
         """
         Click at the given (x, y) coordinate
         """
         screen = win32gui.FindWindowEx(self.bluestacks_id, None, None, None)
-        lParam = MAKELONG(round(x), round(y))
-        win32gui.SendMessage(screen, WM_LBUTTONDOWN, MK_LBUTTON, lParam)
-        win32gui.SendMessage(screen, WM_LBUTTONUP, None, lParam)
+        l_param = MAKELONG(round(x), round(y))
+        win32gui.SendMessage(screen, WM_LBUTTONDOWN, MK_LBUTTON, l_param)
+        win32gui.SendMessage(screen, WM_LBUTTONUP, None, l_param)
+
+    def reset(self):
+        """
+        Reset the size and position of the emulator
+        """
+        self._set_foreground_window()
+        self._move_and_resize_emulator()
 
 
 def main():
     cls = Screen()
-    cls.move_and_resize_emulator()
+    cls.reset()
     screenshot = cls.take_screenshot()
     print(np.array(screenshot).shape)
     screenshot.save('screen.jpg')
