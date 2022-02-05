c73098818f7f017544e31c6b77a27a62c6818798
Pbatch <peter_batchelor1@hotmail.com>
2022-02-05 14:53:31 +0000

    Moved DATA_DIR to constants

--- src/state/detector.py
+++ src/state/detector.py
@@ -1,11 +1,9 @@
 from PIL import Image, ImageDraw
 from src.state.card_detector import CardDetector
 from src.state.number_detector import NumberDetector
-from src.state.hp_detector import HPDetector
 from src.state.unit_detector import UnitDetector
-import os
-
-DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+from src.state.screen_detector import ScreenDetector
+from src.data.constants import DATA_DIR
 
 
 class Detector:
@@ -16,32 +14,27 @@ class Detector:
         self.card_names = card_names
 
         self.card_detector = CardDetector(self.card_names)
-        self.number_detector = NumberDetector()
+        self.number_detector = NumberDetector(f'{DATA_DIR}/number.onnx')
         self.unit_detector = UnitDetector(f'{DATA_DIR}/unit.onnx')
-        self.hp_detector = HPDetector(f'{DATA_DIR}/hp.onnx')
+        self.screen_detector = ScreenDetector()
 
     def run(self, image, debug=False):
         state = {'units': self.unit_detector.run(image),
-                 'hp': self.hp_detector.run(image),
                  'numbers': self.number_detector.run(image),
-                 'cards': self.card_detector.run(image)}
+                 'cards': self.card_detector.run(image),
+                 'screen': self.screen_detector.run(image)}
 
         if debug:
             d = ImageDraw.Draw(image)
 
             for k, v in state['numbers'].items():
                 d.rectangle(tuple(v['bounding_box']))
-                d.text((v['bounding_box'][0], v['bounding_box'][3] + 2), text=v['number'])
+                d.text((v['bounding_box'][0], v['bounding_box'][3] + 2), text=str(v['number']))
 
             for k, v in state['units'].items():
                 for i in v:
                     d.rectangle(tuple(i['bounding_box']))
                     d.text((i['bounding_box'][0], i['bounding_box'][3] + 2), text=k)
-
-            for v in state['hp'].values():
-                d.rectangle(tuple(v['bounding_box']),
-                            outline='white')
-                d.text((v['bounding_box'][0], v['bounding_box'][3] + 2), text=v['hp'])
             image.save('debug.jpg')
 
         return state
@@ -50,7 +43,7 @@ class Detector:
 def main():
     from pprint import pprint
 
-    image_path = '../../screenshots/21.jpg'
+    image_path = '../../screenshots/19.jpg'
     card_names = ['minions', 'archers', 'arrows', 'giant',
                   'minipekka', 'fireball', 'knight', 'musketeer']
 
