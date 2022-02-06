3edc4232d1fd7ca3a961fddb3072faf8d38eba7b
Pbatch <peter_batchelor1@hotmail.com>
2022-02-06 15:04:47 +0000

    Updating constants

--- src/data/constants.py
+++ src/data/constants.py
@@ -34,7 +34,7 @@ SCREEN_CONFIG = [
      (110, 425)],
     ['end_of_game',
      (OK_X, OK_Y, OK_X + OK_WIDTH, OK_Y + OK_HEIGHT),
-     (OK_X + OK_WIDTH/2, OK_Y + OK_HEIGHT/2)]
+     (OK_X + OK_WIDTH / 2, OK_Y + OK_HEIGHT / 2)]
 ]
 
 """
@@ -45,8 +45,8 @@ _H = 7
 KING_HP_X = 188
 LEFT_PRINCESS_HP_X = 74
 RIGHT_PRINCESS_HP_X = 266
-ALLY_PRINCESS_HP_Y = 405
-ENEMY_PRINCESS_HP_Y = 97
+ALLY_PRINCESS_HP_Y = 401
+ENEMY_PRINCESS_HP_Y = 93
 ALLY_KING_LEVEL_Y = 487
 ENEMY_KING_LEVEL_Y = 19
 KING_LEVEL_X = 134
@@ -59,10 +59,14 @@ NUMBER_CONFIG = [
     ['ally_king_level_2', (KING_LEVEL_2_X, ALLY_KING_LEVEL_Y, KING_LEVEL_2_X + _W, ALLY_KING_LEVEL_Y + _H)],
     ['enemy_king_hp', (KING_HP_X, 15, 188 + _W, 15 + _H)],
     ['ally_king_hp', (KING_HP_X, 495, 188 + _W, 495 + _H)],
-    ['right_ally_princess_hp', (RIGHT_PRINCESS_HP_X, ALLY_PRINCESS_HP_Y, RIGHT_PRINCESS_HP_X + _W, ALLY_PRINCESS_HP_Y + _H)],
-    ['left_ally_princess_hp', (LEFT_PRINCESS_HP_X, ALLY_PRINCESS_HP_Y, LEFT_PRINCESS_HP_X + _W, ALLY_PRINCESS_HP_Y + _H)],
-    ['right_enemy_princess_hp', (RIGHT_PRINCESS_HP_X, ENEMY_PRINCESS_HP_Y, RIGHT_PRINCESS_HP_X + _W, ENEMY_PRINCESS_HP_Y + _H)],
-    ['left_enemy_princess_hp', (LEFT_PRINCESS_HP_X, ENEMY_PRINCESS_HP_Y, LEFT_PRINCESS_HP_X + _W, ENEMY_PRINCESS_HP_Y + _H)],
+    ['right_ally_princess_hp',
+     (RIGHT_PRINCESS_HP_X, ALLY_PRINCESS_HP_Y, RIGHT_PRINCESS_HP_X + _W, ALLY_PRINCESS_HP_Y + _H)],
+    ['left_ally_princess_hp',
+     (LEFT_PRINCESS_HP_X, ALLY_PRINCESS_HP_Y, LEFT_PRINCESS_HP_X + _W, ALLY_PRINCESS_HP_Y + _H)],
+    ['right_enemy_princess_hp',
+     (RIGHT_PRINCESS_HP_X, ENEMY_PRINCESS_HP_Y, RIGHT_PRINCESS_HP_X + _W, ENEMY_PRINCESS_HP_Y + _H)],
+    ['left_enemy_princess_hp',
+     (LEFT_PRINCESS_HP_X, ENEMY_PRINCESS_HP_Y, LEFT_PRINCESS_HP_X + _W, ENEMY_PRINCESS_HP_Y + _H)],
 ]
 
 """
@@ -82,7 +86,7 @@ CARD_CONFIG = [
 ]
 
 """
-King and Princess HP for levels 1 to 13
+King and Princess HP for levels 1 to 14
 """
 KING_HP = [2400, 2568, 2736, 2904, 3096, 3312, 3528, 3768, 4008, 4392, 4824, 5304, 5832, 6408]
 PRINCESS_HP = [1400, 1512, 1624, 1750, 1890, 2030, 2184, 2352, 2534, 2786, 3052, 3346, 3668, 4032]
@@ -114,27 +118,50 @@ RIGHT_PRINCESS_TILES += [[x, y]
 Unit classes
 """
 UNITS = [
-    'ally_archer',
-    'ally_giant',
-    'ally_knight',
-    'ally_minion',
-    'ally_minipekka',
-    'ally_musketeer',
-    'enemy_archer',
-    'enemy_brawler',
-    'enemy_giant',
-    'enemy_goblin',
-    'enemy_goblin_cage',
-    'enemy_knight',
-    'enemy_minion',
-    'enemy_minipekka',
-    'enemy_muskateer',
-    'enemy_prince',
-    'enemy_skeleton',
-    'enemy_spear_goblin'
+    "ally_archer",
+    "ally_brawler",
+    "ally_giant",
+    "ally_goblin_cage",
+    "ally_hungry_dragon",
+    "ally_knight",
+    "ally_minion",
+    "ally_minipekka",
+    "ally_musketeer",
+    "ally_prince",
+    "ally_valkyrie",
+    "enemy_archer",
+    "enemy_brawler",
+    "enemy_giant",
+    "enemy_goblin",
+    "enemy_goblin_cage",
+    "enemy_hungry_dragon",
+    "enemy_knight",
+    "enemy_minion",
+    "enemy_minipekka",
+    "enemy_muskateer",
+    "enemy_prince",
+    "enemy_skeleton",
+    "enemy_spear_goblin",
+    "enemy_valkyrie"
 ]
 
 """
 Directories
 """
-DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+SRC_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(SRC_DIR, 'data')
+SCREENSHOTS_DIR = os.path.join(SRC_DIR, 'screenshots')
+
+"""
+Number detector
+"""
+NUMBER_HEIGHT = 16
+NUMBER_WIDTH = 64
+NUMBER_MIN_CONFIDENCE = 0.5
+
+"""
+Unit detector
+"""
+UNIT_SIZE = 416
+UNIT_Y_START = 0.05
+UNIT_Y_END = 0.80
