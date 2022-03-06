6067e64310948cd9b76b2e808d3a2a95f688f3ab
Pbatch <peter_batchelor1@hotmail.com>
2022-03-06 13:33:50 +0000

    Removed torch dependency

--- src/state/onnx_detector.py
+++ src/state/onnx_detector.py
@@ -1,6 +1,4 @@
 import onnxruntime
-import torch
-import torchvision
 import numpy as np
 
 
@@ -13,126 +11,72 @@ class OnnxDetector:
         self.input_name = self.sess.get_inputs()[0].name
 
     @staticmethod
-    def xywh2xyxy(x):
-        y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-        y[:, 0] = x[:, 0] - x[:, 2] / 2
-        y[:, 1] = x[:, 1] - x[:, 3] / 2
-        y[:, 2] = x[:, 0] + x[:, 2] / 2
-        y[:, 3] = x[:, 1] + x[:, 3] / 2
-        return y
+    def _xywh_to_xyxy(boxes):
+        boxes[:, 0] -= boxes[:, 2] / 2
+        boxes[:, 1] -= boxes[:, 3] / 2
+        boxes[:, 2] += boxes[:, 0]
+        boxes[:, 3] += boxes[:, 1]
 
     @staticmethod
-    def box_area(box):
-        box_area = (box[2] - box[0]) * (box[3] - box[1])
-        return box_area
+    def _nms(boxes, scores, thresh):
+        x1, y1, x2, y2 = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
 
-    @staticmethod
-    def xyxy2xywh(x):
-        y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-        y[:, 0] = (x[:, 0] + x[:, 2]) / 2
-        y[:, 1] = (x[:, 1] + x[:, 3]) / 2
-        y[:, 2] = x[:, 2] - x[:, 0]
-        y[:, 3] = x[:, 3] - x[:, 1]
-        return y
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+        order = scores.argsort()[::-1]  # get boxes with more ious first
 
-    def box_iou(self, box1, box2):
-        """
-        Return intersection-over-union (Jaccard index) of boxes.
-        Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
-        """
-        area1 = self.box_area(box1.T)
-        area2 = self.box_area(box2.T)
+        keep = []
+        while order.size > 0:
+            i = order[0]  # pick maximum iou box
+            keep.append(i)
+            xx1 = np.maximum(x1[i], x1[order[1:]])
+            yy1 = np.maximum(y1[i], y1[order[1:]])
+            xx2 = np.minimum(x2[i], x2[order[1:]])
+            yy2 = np.minimum(y2[i], y2[order[1:]])
 
-        inter = (torch.min(box1[:, None, 2:], box2[:, 2:])
-                 - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
-        iou = inter / (area1[:, None] + area2 - inter)
-        return iou
+            w = np.maximum(0.0, xx2 - xx1 + 1)  # maximum width
+            h = np.maximum(0.0, yy2 - yy1 + 1)  # maximum height
+            inter = w * h
+            ovr = inter / (areas[i] + areas[order[1:]] - inter)
 
-    def nms(self, prediction,
-            conf_thres=0.25,
-            iou_thres=0.45,
-            classes=None,
-            agnostic=False,
-            multi_label=False,
-            labels=(),
-            max_det=300):
+            inds = np.where(ovr <= thresh)[0]
+            order = order[inds + 1]
+
+        return keep
+
+    def nms(self, prediction, conf_thres=0.25, iou_thres=0.45):
         """
         Runs Non-Maximum Suppression (NMS) on inference results
         """
-        prediction = torch.tensor(prediction)
-        nc = prediction.shape[2] - 5  # number of classes
-        xc = prediction[..., 4] > conf_thres  # candidates
+        output = []
+        for i in range(prediction.shape[0]):
+            # Mask out predictions below the confidence threshold
+            mask = prediction[i, :, 4] > conf_thres
+            x = prediction[i][mask]
 
-        # Checks
-        assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
-        assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
-
-        # Settings
-        min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
-        max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
-        redundant = True  # require redundant detections
-        multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-        merge = False  # use merge-NMS
-
-        output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
-        for xi, x in enumerate(prediction):  # image index, image inference
-            # Apply constraints
-            # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
-            x = x[xc[xi]]  # confidence
-
-            # Cat apriori labels if autolabelling
-            if labels and len(labels[xi]):
-                l = labels[xi]
-                v = torch.zeros((len(l), nc + 5), device=x.device)
-                v[:, :4] = l[:, 1:5]  # box
-                v[:, 4] = 1.0  # conf
-                v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
-                x = torch.cat((x, v), 0)
-
-            # If none remain process next image
             if not x.shape[0]:
                 continue
 
-            # Compute conf
-            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+            # Calculate the best scores
+            # score = object confidence * class confidence
+            scores = x[:, 4:5] * x[:, 5:]
+            best_scores_idx = np.argmax(scores, axis=1).reshape(-1, 1)
+            best_scores = np.take_along_axis(scores, best_scores_idx, axis=1)
 
-            # Box (center x, center y, width, height) to (x1, y1, x2, y2)
-            box = self.xywh2xyxy(x[:, :4])
+            # Again, mask out predictions below the confidence threshold
+            mask = np.ravel(best_scores > conf_thres)
+            best_scores = best_scores[mask]
+            best_scores_idx = best_scores_idx[mask]
 
-            # Detections matrix nx6 (xyxy, conf, cls)
-            if multi_label:
-                i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
-                x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
-            else:  # best class only
-                conf, j = x[:, 5:].max(1, keepdim=True)
-                x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+            # Convert the xywh of each box to xyxy inplace
+            boxes = x[mask, :4]
+            self._xywh_to_xyxy(boxes)
 
-            # Filter by class
-            if classes is not None:
-                x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+            # Work out which boxes to keep
+            keep = self._nms(boxes, np.ravel(best_scores), iou_thres)
 
-            # Check shape
-            n = x.shape[0]  # number of boxes
-            if not n:  # no boxes
-                continue
-            elif n > max_nms:  # excess boxes
-                x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
-
-            # Batched NMS
-            c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
-            boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-            i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-            if i.shape[0] > max_det:  # limit detections
-                i = i[:max_det]
-            if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
-                # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-                iou = self.box_iou(boxes[i], boxes) > iou_thres  # iou matrix
-                weights = iou * scores[None]  # box weights
-                x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
-                if redundant:
-                    i = i[iou.sum(1) > 1]  # require redundancy
-
-            output[xi] = x[i]
+            # Keep only the best class
+            best = np.hstack([boxes[keep], best_scores[keep], best_scores_idx[keep]])
+            output.append(best)
 
         return output
 
