dbd2f0cbf76751876e9c4b71c40dc9ed810d0578
Pbatch <peter_batchelor1@hotmail.com>
2022-03-20 23:05:47 +0000

    Add new display constants

--- src/data/constants.py
+++ src/data/constants.py
@@ -1,23 +1,25 @@
 import os
 
-from win32api import GetSystemMetrics
+"""
+Miscellaneous
+"""
+# Directories
+SRC_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(SRC_DIR, 'data')
+SCREENSHOTS_DIR = os.path.join(SRC_DIR, 'screenshots')
 
 """
-Screen dimensions
+Click config
 """
-SCREEN_WIDTH = GetSystemMetrics(0)
-SCREEN_HEIGHT = GetSystemMetrics(1)
+# Display dimensions
+DISPLAY_WIDTH = 720
+DISPLAY_HEIGHT = 1280
 
-"""
-App dimensions
-"""
-APP_WIDTH = 400
-APP_HEIGHT = 684
-BORDER_SIZE = 32
+# Screenshot dimensions
+SCREENSHOT_WIDTH = 368
+SCREENSHOT_HEIGHT = 652
 
-"""
-Bounding boxes for screen ID
-"""
+# Screen ID
 CHEST_SIZE = 62
 CHEST_X = 0
 CHEST_Y = 590
@@ -31,15 +33,61 @@ SCREEN_CONFIG = [
       CHEST_Y,
       CHEST_X + CHEST_SIZE,
       CHEST_Y + CHEST_SIZE),
-     (110, 425)],
+     (220, 830)],
     ['end_of_game',
      (OK_X, OK_Y, OK_X + OK_WIDTH, OK_Y + OK_HEIGHT),
-     (OK_X + OK_WIDTH / 2, OK_Y + OK_HEIGHT / 2)]
+     (360, 1125)]
 ]
+
+# Playable tiles
+TILE_HEIGHT = 27.6
+TILE_WIDTH = 34
+N_HEIGHT_TILES = 15
+N_WIDE_TILES = 18
+TILE_INIT_X = 52
+TILE_INIT_Y = 296
+ALLY_TILES = [[x, 0]
+              for x in range(N_WIDE_TILES // 3, 2 * N_WIDE_TILES // 3)]
+ALLY_TILES += [[x, y]
+               for x in range(N_WIDE_TILES)
+               for y in range(1, N_HEIGHT_TILES)]
+LEFT_PRINCESS_TILES = [[3, N_HEIGHT_TILES], [3, N_HEIGHT_TILES + 1]]
+LEFT_PRINCESS_TILES += [[x, y]
+                        for x in range(N_WIDE_TILES // 2)
+                        for y in range(N_HEIGHT_TILES + 2, N_HEIGHT_TILES + 6)]
+RIGHT_PRINCESS_TILES = [[14, N_HEIGHT_TILES], [14, N_HEIGHT_TILES + 1]]
+RIGHT_PRINCESS_TILES += [[x, y]
+                         for x in range(N_WIDE_TILES // 2, N_WIDE_TILES)
+                         for y in range(N_HEIGHT_TILES + 2, N_HEIGHT_TILES + 6)]
+DISPLAY_CARD_Y = 1067
+DISPLAY_CARD_INIT_X = 164
+DISPLAY_CARD_WIDTH = 117
+DISPLAY_CARD_HEIGHT = 147
+DISPLAY_CARD_DELTA_X = 136
 
 """
 Bounding boxes for numbers
 """
+
+
+"""
+Detector config
+"""
+# Cards
+CARD_Y = 545
+CARD_INIT_X = 87
+CARD_WIDTH = 55
+CARD_HEIGHT = 65
+CARD_DELTA_X = 69
+CARD_CONFIG = [
+    (19, 605, 51, 645),
+    (CARD_INIT_X, CARD_Y, CARD_INIT_X + CARD_WIDTH, CARD_Y + CARD_HEIGHT),
+    (CARD_INIT_X + CARD_DELTA_X, CARD_Y, CARD_INIT_X + CARD_WIDTH + CARD_DELTA_X, CARD_Y + CARD_HEIGHT),
+    (CARD_INIT_X + 2 * CARD_DELTA_X, CARD_Y, CARD_INIT_X + CARD_WIDTH + 2 * CARD_DELTA_X, CARD_Y + CARD_HEIGHT),
+    (CARD_INIT_X + 3 * CARD_DELTA_X, CARD_Y, CARD_INIT_X + CARD_WIDTH + 3 * CARD_DELTA_X, CARD_Y + CARD_HEIGHT),
+]
+
+# Numbers
 _W = 28
 _H = 7
 KING_HP_X = 188
@@ -68,55 +116,18 @@ NUMBER_CONFIG = [
     ['left_enemy_princess_hp',
      (LEFT_PRINCESS_HP_X, ENEMY_PRINCESS_HP_Y, LEFT_PRINCESS_HP_X + _W, ENEMY_PRINCESS_HP_Y + _H)],
 ]
+NUMBER_HEIGHT = 16
+NUMBER_WIDTH = 64
+NUMBER_MIN_CONFIDENCE = 0.5
 
-"""
-Bounding boxes for cards
-"""
-CARD_Y = 545
-CARD_INIT_X = 87
-CARD_WIDTH = 55
-CARD_HEIGHT = 65
-CARD_DELTA_X = 69
-CARD_CONFIG = [
-    (19, 605, 51, 645),
-    (CARD_INIT_X, CARD_Y, CARD_INIT_X + CARD_WIDTH, CARD_Y + CARD_HEIGHT),
-    (CARD_INIT_X + CARD_DELTA_X, CARD_Y, CARD_INIT_X + CARD_WIDTH + CARD_DELTA_X, CARD_Y + CARD_HEIGHT),
-    (CARD_INIT_X + 2 * CARD_DELTA_X, CARD_Y, CARD_INIT_X + CARD_WIDTH + 2 * CARD_DELTA_X, CARD_Y + CARD_HEIGHT),
-    (CARD_INIT_X + 3 * CARD_DELTA_X, CARD_Y, CARD_INIT_X + CARD_WIDTH + 3 * CARD_DELTA_X, CARD_Y + CARD_HEIGHT),
-]
-
-"""
-King and Princess HP for levels 1 to 14
-"""
+# HP
 KING_HP = [2400, 2568, 2736, 2904, 3096, 3312, 3528, 3768, 4008, 4392, 4824, 5304, 5832, 6408]
 PRINCESS_HP = [1400, 1512, 1624, 1750, 1890, 2030, 2184, 2352, 2534, 2786, 3052, 3346, 3668, 4032]
 
-"""
-Playable tiles
-"""
-TILE_HEIGHT = 14
-TILE_WIDTH = 17
-N_HEIGHT_TILES = 15
-N_WIDE_TILES = 18
-LEFT_PAD = 30
-LOWER_PAD = 152
-ALLY_TILES = [[x, 0]
-              for x in range(N_WIDE_TILES // 3, 2 * N_WIDE_TILES // 3)]
-ALLY_TILES += [[x, y]
-               for x in range(N_WIDE_TILES)
-               for y in range(1, N_HEIGHT_TILES)]
-LEFT_PRINCESS_TILES = [[3, N_HEIGHT_TILES], [3, N_HEIGHT_TILES + 1]]
-LEFT_PRINCESS_TILES += [[x, y]
-                        for x in range(N_WIDE_TILES // 2)
-                        for y in range(N_HEIGHT_TILES + 2, N_HEIGHT_TILES + 6)]
-RIGHT_PRINCESS_TILES = [[14, N_HEIGHT_TILES], [14, N_HEIGHT_TILES + 1]]
-RIGHT_PRINCESS_TILES += [[x, y]
-                         for x in range(N_WIDE_TILES // 2, N_WIDE_TILES)
-                         for y in range(N_HEIGHT_TILES + 2, N_HEIGHT_TILES + 6)]
-
-"""
-Unit classes
-"""
+# Units
+UNIT_SIZE = 416
+UNIT_Y_START = 0.05
+UNIT_Y_END = 0.80
 UNITS = [
     "ally_archer",
     "ally_brawler",
@@ -144,24 +155,3 @@ UNITS = [
     "enemy_spear_goblin",
     "enemy_valkyrie"
 ]
-
-"""
-Directories
-"""
-SRC_DIR = os.path.dirname(os.path.dirname(__file__))
-DATA_DIR = os.path.join(SRC_DIR, 'data')
-SCREENSHOTS_DIR = os.path.join(SRC_DIR, 'screenshots')
-
-"""
-Number detector
-"""
-NUMBER_HEIGHT = 16
-NUMBER_WIDTH = 64
-NUMBER_MIN_CONFIDENCE = 0.5
-
-"""
-Unit detector
-"""
-UNIT_SIZE = 416
-UNIT_Y_START = 0.05
-UNIT_Y_END = 0.80
