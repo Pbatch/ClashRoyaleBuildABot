aa219792b963fde3b31d1598e0ce960b0e5ed3e9
Pbatch <peter_batchelor1@hotmail.com>
2022-07-02 18:24:45 +0100

    Up the confidence threshold to reduce false positives

--- clashroyalebuildabot/state/onnx_detector.py
+++ clashroyalebuildabot/state/onnx_detector.py
@@ -43,7 +43,7 @@ class OnnxDetector:
 
         return keep
 
-    def nms(self, prediction, conf_thres=0.25, iou_thres=0.45):
+    def nms(self, prediction, conf_thres=0.35, iou_thres=0.45):
         """
         Runs Non-Maximum Suppression (NMS) on inference results
         """
