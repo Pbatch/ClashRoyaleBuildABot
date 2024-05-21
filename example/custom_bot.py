ffcdebed74ec25a46d943edcc6c617297d06e0eb
Leviaria <113382526+Leviaria@users.noreply.github.com>
2024-05-21 23:00:08 +0200

    Update custom_bot.py
    Added Keyboard Interrupt

--- example/custom_bot.py
+++ example/custom_bot.py
@@ -7,8 +7,9 @@ from clashroyalebuildabot.bot import Bot
 from custom_action import CustomAction
 from clashroyalebuildabot.data.constants import DISPLAY_WIDTH, SCREENSHOT_WIDTH, DISPLAY_HEIGHT, SCREENSHOT_HEIGHT
 from clashroyalebuildabot.data.constants import SCREEN_CONFIG
-from loguru import logger 
+from loguru import logger
 from pathlib import Path
+
 
 class CustomBot(Bot):
     def __init__(self, card_names, debug=False):
@@ -18,7 +19,7 @@ class CustomBot(Bot):
         super().__init__(card_names, CustomAction, debug=debug)
         self.end_of_game_clicked = False
         self.pause_until = 0
-        
+
     def _preprocess(self):
         for side in ['ally', 'enemy']:
             for k, v in self.state['units'][side].items():
@@ -32,47 +33,51 @@ class CustomBot(Bot):
                     unit['tile_xy'] = self._get_nearest_tile(*bbox_bottom)
 
     def run(self):
-        while True:
-            if self.end_of_game_clicked:
-                if time.time() > self.pause_until:
-                    if not any(action.name == "Battle" for action in self.get_actions()):
-                        logger.info("Cant find Battle button, force game restart.")  # Loguru logging
-                        subprocess.run("adb shell am force-stop com.supercell.clashroyale", shell=True)
-                        time.sleep(1)
-                        subprocess.run("adb shell am start -n com.supercell.clashroyale/com.supercell.titan.GameApp", shell=True)
+        try:
+            while True:
+                if self.end_of_game_clicked:
+                    if time.time() > self.pause_until:
+                        if not any(action.name == "Battle" for action in self.get_actions()):
+                            logger.info("Can't find Battle button, force game restart.")
+                            subprocess.run("adb shell am force-stop com.supercell.clashroyale", shell=True)
+                            time.sleep(1)
+                            subprocess.run("adb shell am start -n com.supercell.clashroyale/com.supercell.titan.GameApp", shell=True)
 
-                        logger.info("Waiting 10 seconds...")  # Loguru logging
-                        time.sleep(10)
+                            logger.info("Waiting 10 seconds...")
+                            time.sleep(10)
 
-                    self.end_of_game_clicked = False
-                else:
+                        self.end_of_game_clicked = False
+                    else:
+                        time.sleep(1.0)
+                        continue
+
+                self.set_state()
+
+                if self.state['screen'] == 'end_of_game':
+                    logger.info("End of game detected. Waiting 10 seconds for battle button...")
+                    self.pause_until = time.time() + 10
+                    self.end_of_game_clicked = True
+                    time.sleep(10)
+
+                actions = self.get_actions()
+
+                if not actions:
+                    if self.debug:
+                        logger.debug("No actions available. Waiting for 1 second...")
                     time.sleep(1.0)
                     continue
+                elif self.state['screen'] != 'lobby':
+                    random.shuffle(actions)
+                    self._preprocess()
+                    action = max(actions, key=lambda x: x.calculate_score(self.state))
+                    if action.score[0] == 0:
+                        continue
+                    self.play_action(action)
+                    logger.info(f"Playing {action} with score {action.score} and sleeping for 1 second")
+                    time.sleep(1.0)
+                else:
+                    logger.info("In the main menu or no actions available. Waiting for 1 second...")
+                    time.sleep(1.0)
 
-            self.set_state()
-
-            if self.state['screen'] == 'end_of_game':
-                logger.info("End of game detected. Waiting 10 seconds for battle button...")  # Loguru logging
-                self.pause_until = time.time() + 10
-                self.end_of_game_clicked = True
-                time.sleep(10)
-
-            actions = self.get_actions()
-
-            if not actions:
-                if self.debug:
-                    logger.debug("No actions available. Waiting for 1 second...")  # Loguru logging (debug level)
-                time.sleep(1.0)
-                continue
-            elif self.state['screen'] != 'lobby':
-                random.shuffle(actions)
-                self._preprocess()
-                action = max(actions, key=lambda x: x.calculate_score(self.state))
-                if action.score[0] == 0:
-                    continue
-                self.play_action(action)
-                logger.info(f"Playing {action} with score {action.score} and sleeping for 1 second") # Loguru logging
-                time.sleep(1.0)
-            else:
-                logger.info("In the main menu or no actions available. Waiting for 1 second...")  # Loguru logging
-                time.sleep(1.0)
+        except KeyboardInterrupt:
+            logger.info("KeyboardInterrupt detected. Exiting bot gracefully.")  # Log the interruption
