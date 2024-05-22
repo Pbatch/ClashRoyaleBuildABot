1f8bbb9c91e52e9d64321ee18bd4933363b2339a
Leviaria <113382526+Leviaria@users.noreply.github.com>
2024-05-22 23:03:57 +0200

    Update main.py

--- example/main.py
+++ example/main.py
@@ -1,27 +1,20 @@
 """
 A CustomBot implementation through import
 """
-
-from custom_bot import CustomBot  # see custom_bot.py
+from custom_bot import CustomBot # see custom_bot.py
+from clashroyalebuildabot.state.error_handler import adb_fix
 
 
 def main():
+    adb_fix()
     # Set required bot variables
-    card_names = [
-        "minions",
-        "archers",
-        "arrows",
-        "giant",
-        "minipekka",
-        "fireball",
-        "knight",
-        "musketeer",
-    ]
+    card_names = ['minions', 'archers', 'arrows', 'giant',
+                  'minipekka', 'fireball', 'knight', 'musketeer']
     # Define an instance of CustomBot
-    bot = CustomBot(card_names, debug=True)
+    bot = CustomBot(card_names, debug=False)
     # and run!
     bot.run()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
