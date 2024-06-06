d1fc11fc328cfa6f1e9d5337da9b727ca9ca0909
Leviaria <113382526+Leviaria@users.noreply.github.com>
2024-06-06 19:05:18 +0200

    Update side_detector.py

--- clashroyalebuildabot/state/side_detector.py
+++ clashroyalebuildabot/state/side_detector.py
@@ -1,30 +1,19 @@
 import numpy as np
 from PIL import Image
-
 from clashroyalebuildabot.data.constants import SIDE_SIZE
 from clashroyalebuildabot.state.onnx_detector import OnnxDetector
-
 
 class SideDetector(OnnxDetector):
     @staticmethod
     def _preprocess(image):
         image = image.resize((SIDE_SIZE, SIDE_SIZE), Image.BICUBIC)
-        image = np.array(image, dtype=np.float32)
-        image = image / 255
-        image = np.expand_dims(image, axis=0)
-        return image
+        image = np.array(image, dtype=np.float32) / 255
+        return np.expand_dims(image, axis=0)
 
     def _post_process(self, pred):
         return ("ally", "enemy")[np.argmax(pred[0])]
 
     def run(self, image):
-        # Preprocessing
         image = self._preprocess(image)
-
-        # Inference
         pred = self._infer(image)
-
-        # Postprocessing
-        side = self._post_process(pred)
-
-        return side
+        return self._post_process(pred)
