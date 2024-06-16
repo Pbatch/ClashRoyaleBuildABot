787de6506b4136d2d87226d73471f931ea4ff20c
Pbatch <37177749+Pbatch@users.noreply.github.com>
2024-06-16 02:24:00 +0100

    Make debugging prettier (#145)
    * Add debug class
    
    * Add debugger
    
    * Move debugging out into its own class
    
    * Move static method to the top of the class
    
    * Remove main method
    
    * Remove redundant min_conf argument

---
+++ clashroyalebuildabot/state/debugger.py
@@ -0,0 +1,93 @@
+import os
+
+from PIL import ImageDraw, ImageFont
+
+from clashroyalebuildabot.data.constants import (
+    LABELS_DIR,
+    SCREENSHOTS_DIR,
+    CARD_CONFIG,
+    UNITS,
+)
+
+
+class Debugger:
+    _COLOUR_AND_RGBA = [
+        ["navy", (0, 38, 63, 127)],
+        ["blue", (0, 120, 210, 127)],
+        ["aqua", (115, 221, 252, 127)],
+        ["teal", (15, 205, 202, 127)],
+        ["olive", (52, 153, 114, 127)],
+        ["green", (0, 204, 84, 127)],
+        ["lime", (1, 255, 127, 127)],
+        ["yellow", (255, 216, 70, 127)],
+        ["orange", (255, 125, 57, 127)],
+        ["red", (255, 47, 65, 127)],
+        ["maroon", (135, 13, 75, 127)],
+        ["fuchsia", (246, 0, 184, 127)],
+        ["purple", (179, 17, 193, 127)],
+        ["gray", (168, 168, 168, 127)],
+        ["silver", (220, 220, 220, 127)],
+    ]
+
+    def __init__(self):
+        os.makedirs(SCREENSHOTS_DIR, exist_ok=True)
+        os.makedirs(LABELS_DIR, exist_ok=True)
+        self.font = ImageFont.load_default()
+
+    @staticmethod
+    def _write_label(image, state, basename):
+        labels = []
+        for side in ["ally", "enemy"]:
+            for unit_name, v in state["units"][side].items():
+                for i in v["positions"]:
+                    bbox = i["bounding_box"]
+                    xc = (bbox[0] + bbox[2]) / (2 * image.width)
+                    yc = (bbox[1] + bbox[3]) / (2 * image.height)
+                    w = (bbox[2] - bbox[0]) / image.width
+                    h = (bbox[3] - bbox[1]) / image.height
+                    label = f"{unit_name} {xc} {yc} {w} {h}"
+                    labels.append(label)
+
+        with open(os.path.join(LABELS_DIR, f"{basename}.txt"), "w") as f:
+            f.write("\n".join(labels))
+
+    def _draw_text(self, d, bbox, text, rgba=(0, 0, 0, 255)):
+        text_width, text_height = d.textbbox((0, 0), text, font=self.font)[2:]
+        text_bbox = (
+            bbox[0],
+            bbox[1] - text_height,
+            bbox[0] + text_width,
+            bbox[1],
+        )
+        d.rectangle(text_bbox, fill=rgba)
+        d.rectangle(tuple(bbox), outline=rgba)
+        d.text(tuple(text_bbox[:2]), text=text, fill="white")
+
+    def _write_image(self, image, state, basename):
+        d = ImageDraw.Draw(image, "RGBA")
+        for k, v in state["numbers"].items():
+            d.rectangle(tuple(v["bounding_box"]))
+            self._draw_text(d, v["bounding_box"], str(v["number"]))
+
+        for side in ["ally", "enemy"]:
+            for unit_name, v in state["units"][side].items():
+                colour_idx = UNITS.index(unit_name) % len(
+                    self._COLOUR_AND_RGBA
+                )
+                rgba = self._COLOUR_AND_RGBA[colour_idx][1]
+                for i in v["positions"]:
+                    self._draw_text(d, i["bounding_box"], unit_name, rgba)
+
+        for card, position in zip(state["cards"], CARD_CONFIG):
+            d.rectangle(tuple(position))
+            self._draw_text(d, position, card["name"])
+
+        image.save(os.path.join(SCREENSHOTS_DIR, f"{basename}.png"))
+
+    def run(self, image, state):
+        n_screenshots = len(os.listdir(SCREENSHOTS_DIR))
+        n_labels = len(os.listdir(LABELS_DIR))
+        basename = max(n_labels, n_screenshots) + 1
+
+        self._write_image(image, state, basename)
+        self._write_label(image, state, basename)
--- clashroyalebuildabot/state/detector.py
+++ clashroyalebuildabot/state/detector.py
@@ -1,13 +1,11 @@
 import os
-from PIL import ImageDraw, ImageFont
+
 from clashroyalebuildabot.data.constants import (
     DATA_DIR,
-    SCREENSHOTS_DIR,
-    CARD_CONFIG,
     DECK_SIZE,
-    LABELS_DIR,
 )
 from clashroyalebuildabot.state.card_detector import CardDetector
+from clashroyalebuildabot.state.debugger import Debugger
 from clashroyalebuildabot.state.number_detector import NumberDetector
 from clashroyalebuildabot.state.screen_detector import ScreenDetector
 from clashroyalebuildabot.state.side_detector import SideDetector
@@ -15,18 +13,12 @@ from clashroyalebuildabot.state.unit_detector import UnitDetector
 
 
 class Detector:
-    def __init__(self, card_names, debug=False, min_conf=0.5):
+    def __init__(self, card_names, debug=False):
         if len(card_names) != DECK_SIZE:
             raise ValueError(f"You must specify all {DECK_SIZE} of your cards")
 
         self.card_names = card_names
         self.debug = debug
-        self.min_conf = min_conf
-
-        if self.debug:
-            os.makedirs(SCREENSHOTS_DIR, exist_ok=True)
-            os.makedirs(LABELS_DIR, exist_ok=True)
-            self.font = ImageFont.load_default()
 
         self.card_detector = CardDetector(self.card_names)
         self.number_detector = NumberDetector(
@@ -38,22 +30,9 @@ class Detector:
         self.screen_detector = ScreenDetector()
         self.side_detector = SideDetector(os.path.join(DATA_DIR, "side.onnx"))
 
-    def _draw_text(self, d, bbox, text):
-        _, _, text_width, text_height = self.font.getbbox(text)
-        y_offset = 5
-        x = (bbox[0] + bbox[2] - text_width) / 2
-        y = bbox[1] - y_offset
-
-        mid_x = (bbox[0] + bbox[2]) / 2
-        text_bbox = (
-            mid_x - text_width / 2 - 5,
-            bbox[1] - y_offset - text_height,
-            mid_x + text_width / 2 + 5,
-            bbox[1] - y_offset,
-        )
-        d.rectangle(text_bbox, fill="black")
-        d.rectangle(tuple(bbox))
-        d.text((x, y - text_height), text=text)
+        self.debugger = None
+        if self.debug:
+            self.debugger = Debugger()
 
     def run(self, image):
         state = {
@@ -63,41 +42,7 @@ class Detector:
             "screen": self.screen_detector.run(image),
         }
 
-        if self.debug:
-            d = ImageDraw.Draw(image)
-            width, height = image.width, image.height
-            labels = []
-
-            for k, v in state["numbers"].items():
-                d.rectangle(tuple(v["bounding_box"]))
-                self._draw_text(d, v["bounding_box"], str(v["number"]))
-
-            for side in ["ally", "enemy"]:
-                for unit_name, v in state["units"][side].items():
-                    for i in v["positions"]:
-                        bbox = i["bounding_box"]
-                        yolov5_bbox = [
-                            (bbox[0] + bbox[2]) / (2 * width),
-                            (bbox[1] + bbox[3]) / (2 * height),
-                            (bbox[2] - bbox[0]) / width,
-                            (bbox[3] - bbox[1]) / height,
-                        ]
-                        labels.append([unit_name, *yolov5_bbox])
-                        self._draw_text(d, bbox, unit_name)
-
-            for card, position in zip(state["cards"], CARD_CONFIG):
-                d.rectangle(tuple(position))
-                self._draw_text(d, position, card["name"])
-
-            n_screenshots = len(os.listdir(SCREENSHOTS_DIR))
-            n_labels = len(os.listdir(LABELS_DIR))
-            basename = max(n_labels, n_screenshots) + 1
-            image.save(os.path.join(SCREENSHOTS_DIR, f"{basename}.jpg"))
-
-            with open(os.path.join(LABELS_DIR, f"{basename}.txt"), "w") as f:
-                label_string = "\n".join(
-                    [" ".join(map(str, label)) for label in labels]
-                )
-                f.write(label_string)
+        if self.debugger is not None:
+            self.debugger.run(image, state)
 
         return state
