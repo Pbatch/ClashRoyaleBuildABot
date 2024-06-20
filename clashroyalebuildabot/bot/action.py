3599f6c7521357d7ed1c0d93ee632f44588df6c6
Leviaria <113382526+Leviaria@users.noreply.github.com>
2024-06-20 11:43:05 +0200

    Delete clashroyalebuildabot/bot/action.py

--- clashroyalebuildabot/bot/action.py
+++
@@ -1,9 +0,0 @@
-class Action:
-    def __init__(self, index, tile_x, tile_y, card):
-        self.index = index
-        self.tile_x = tile_x
-        self.tile_y = tile_y
-        self.card = card
-
-    def __repr__(self):
-        return f"{self.card.name} at ({self.tile_x}, {self.tile_y})"
