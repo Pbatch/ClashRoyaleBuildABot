d715d88e1794502f0f053517fd73f46d28022672
peter <peter_batchelor1@hotmail.com>
2024-06-22 01:19:16 +0100

    Cleaning...

--- clashroyalebuildabot/namespaces/state.py
+++ clashroyalebuildabot/namespaces/state.py
@@ -7,8 +7,17 @@ from clashroyalebuildabot.namespaces.screens import Screen
 
 @dataclass
 class State:
-    units: Any
+    enemies: Any
+    allies: Any
     numbers: Any
     cards: Tuple[Card, Card, Card, Card]
     ready: List[int]
     screen: Screen
+
+
+@dataclass
+class Position:
+    bbox: Tuple[int, int, int, int]
+    conf: float
+    tile_x: int
+    tile_y: int
