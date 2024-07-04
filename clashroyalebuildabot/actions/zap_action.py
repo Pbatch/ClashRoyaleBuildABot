71f1cd98bfbe28dc742c811f6df3b02f9c0ffc46
Pbatch <37177749+Pbatch@users.noreply.github.com>
2024-07-04 17:39:38 +0100

    Add zap action (#200)

---
+++ clashroyalebuildabot/actions/zap_action.py
@@ -0,0 +1,8 @@
+from clashroyalebuildabot import Cards
+from clashroyalebuildabot.actions.spell_action import SpellAction
+
+
+class ZapAction(SpellAction):
+    CARD = Cards.ZAP
+    RADIUS = 2.5
+    MIN_TO_HIT = 3
