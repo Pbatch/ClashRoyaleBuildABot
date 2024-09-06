13ff08c005a24cbf3f261f5bede3f374ed0f68ad
peter <peter_batchelor1@hotmail.com>
2024-09-06 14:32:47 +0100

    Make hp detection completely unsupervised

--- clashroyalebuildabot/namespaces/numbers.py
+++ clashroyalebuildabot/namespaces/numbers.py
@@ -1,22 +1,17 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import Tuple
 
 
 @dataclass(frozen=True)
 class NumberDetection:
     bbox: Tuple[int, int, int, int]
-    confidence: List[float]
-    number: int
+    number: float
 
 
 @dataclass(frozen=True)
 class Numbers:
-    enemy_king_level: NumberDetection
-    enemy_king_hp: NumberDetection
     left_enemy_princess_hp: NumberDetection
     right_enemy_princess_hp: NumberDetection
-    ally_king_level: NumberDetection
-    ally_king_hp: NumberDetection
     left_ally_princess_hp: NumberDetection
     right_ally_princess_hp: NumberDetection
     elixir: NumberDetection
