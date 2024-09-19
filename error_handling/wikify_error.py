896b8d952b9b6ea1ab576e2ad3ab617ecb337ad1
fe-art <github.rebuild296@aleeas.com>
2024-09-19 21:47:47 +0200

    fix f-string breaking the whole point of this PR

--- error_handling/wikify_error.py
+++ error_handling/wikify_error.py
@@ -1,7 +1,7 @@
 def get_wikified_error_message(error_code: str, reason: str) -> str:
     err_str = f"\u26A0 Error #E{str(error_code)}: {reason}"
     link = "https://github.com/Pbatch/ClashRoyaleBuildABot/wiki/"
-    link += "Troubleshooting#error-e{str(error_code)}"
+    link += f"Troubleshooting#error-e{str(error_code)}"
     err_str += f" See {link} for more information."
     err_str += " You might find more context above this error.\n\n"
     return err_str
