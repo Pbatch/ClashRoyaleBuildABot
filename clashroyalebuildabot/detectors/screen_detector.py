a43674bcf7ebcf0ad9bbbff453616cadc110591b
fe-art <github.rebuild296@aleeas.com>
2024-10-21 19:54:53 +0200

    add new images to detect screens + dimension scaling from screens.py to real screenshot size (to prevent misinterpretation)

--- clashroyalebuildabot/detectors/screen_detector.py
+++ clashroyalebuildabot/detectors/screen_detector.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 from clashroyalebuildabot.constants import IMAGES_DIR
 from clashroyalebuildabot.namespaces import Screens
+from clashroyalebuildabot.namespaces.screens import Screen
 
 
 class ScreenDetector:
@@ -30,15 +31,21 @@ class ScreenDetector:
             screen_hashes[screen] = self._image_hash(image)
         return screen_hashes
 
-    def run(self, image):
+    def run(self, image: Image) -> Screen:
         current_screen = Screens.UNKNOWN
         best_diff = self.threshold
 
         for screen in Screens.__dict__.values():
             if screen.ltrb is None:
                 continue
-
-            hash_ = self._image_hash(image.crop(screen.ltrb))
+            # screen.ltb are dimensions scaled to 720x1280 so we scale them :
+            treated_ltrb = (
+                int(screen.ltrb[0] * image.size[0] / 720),
+                int(screen.ltrb[1] * image.size[1] / 1280),
+                int(screen.ltrb[2] * image.size[0] / 720),
+                int(screen.ltrb[3] * image.size[1] / 1280),
+            )
+            hash_ = self._image_hash(image.crop(treated_ltrb))
             target_hash = self.screen_hashes[screen]
 
             diff = np.mean(np.abs(hash_ - target_hash))
